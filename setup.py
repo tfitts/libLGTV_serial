3edd2998d303ec02014f40955b6b9d59e002ecad
tfitts <tfitts@gmail.com>
2016-05-15 00:31:09 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='liblgtv_serial',
+      version='0.1.0',
+      description='Library to control LG TVs by serial port',
+      py_modules=['liblgtv_serial'])
