ca330fdaca06c17bb79fc01161244d89233cf83c
tfitts <tfitts@gmail.com>
2016-05-14 18:37:57 -0600

    Change library name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='liblgtv_serial',
+setup(name='libLGTV_serial',
       version='0.1.0',
       description='Library to control LG TVs by serial port',
-      py_modules=['liblgtv_serial'])
+      py_modules=['libLGTV_serial'])
